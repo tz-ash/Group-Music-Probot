443b5a0674e9b177ead4d4a1b9e42f1cff68a388
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-08-01 10:13:00 +0530

    Update private.py

--- Group_Music_Probot/modules/private.py
+++ Group_Music_Probot/modules/private.py
@@ -91,7 +91,7 @@ def map(pos):
             [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
             [InlineKeyboardButton(text = '📲 Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
              InlineKeyboardButton(text = '💬 Support', url=f"https://t.me/{SUPPORT_GROUP}")],
-            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://t.me/{SUPPORT_GROUP")],
+            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://t.me/{SUPPORT_GROUP}")],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
     else:
